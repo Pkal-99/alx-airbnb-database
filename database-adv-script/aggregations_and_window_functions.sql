129b166f9879b94f77556a6ba96308494b5a3372
Pkal-99 <kal4ever10q@gmail.com>
2025-11-04 09:48:49 +0300

    aggregation and window file update

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -6,11 +6,20 @@ GROUP BY u.user_id, u.name;
 
 
 --- Rank properties based on the number of bookings (using ROW_NUMBER and RANK)
-SELECT 
-  p.property_id,
-  p.location,
-  COUNT(b.booking_id) AS total_bookings,
-  RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS rank_position
-FROM properties p
-LEFT JOIN bookings b ON p.property_id = b.property_id
-GROUP BY p.property_id, p.location;
+SELECT
+    p.property_id,
+    p.property_name,
+    COUNT(b.booking_id) AS total_bookings,
+    -- 1. Using RANK(): Skips ranks in case of ties.
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property_rank_rank,
+    -- 2. Using ROW_NUMBER(): Assigns a unique rank (no ties).
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property_rank_row_number
+FROM
+    properties p
+JOIN
+    bookings b ON p.property_id = b.property_id
+GROUP BY
+    p.property_id,
+    p.property_name
+ORDER BY
+    total_bookings DESC;
